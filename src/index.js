61d170afc8dc977bcd25a65f666d37f918358019
AMBE BENJAMEN <ambebenjamen5@gmail.com>
2024-08-16 16:37:53 +0100

    first update

---
+++ src/index.js
@@ -0,0 +1,38 @@
+/*let name = 'AMBE';
+let age =23;
+let fevorite = 10;
+const me= ["jen, feb, march, april, may, june , july aug, sep, oct, nov, decem";]
+console.log("name,age, fevorite,me[0]")*/
+  
+
+/*let age=23;
+let myage=365*age;
+console.log(myage)*/
+/*let x=+7
+ let  z = x+7
+console.log(z)*/
+/*let age =23
+y=age+1
+console.log(y)*/
+
+  /*let x=23;
+  let y=30;
+  let z= x==y
+  console.log(z)*/
+   /*let x= ["meat" ,"fish" ,"garri","suop","plantain"];
+   let now=[[x[2]],"conne","beans"]
+   
+   console.log(now)*/
+    /*let x= ["meat" ,"fish" ,"garri","suop","plantain"];
+    let now=[[x[3]],"conne","beans"]
+    
+   console.log(now)*/
+   let x= ["meat" ,"fish" ,"garri","suop","plantain"]
+   let y=["tomato","conne","beans"]
+   x.shift("mam")
+
+console.log(x)
+   /*function nan(x,y){
+    return x + y;
+   }
+   console.log (nan(4,5))8*/
